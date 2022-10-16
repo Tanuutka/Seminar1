35a1563d54edcb57657b5cd7935c9e62e826b580
Tanuutka <Tanuutka@yandex.ru>
2022-10-16 12:42:27 +0300

    написана программа нахождения наибольшего и наименьшего числа

---
+++ Seminar1/Less1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+if (num1 > num2)
+{
+   Console.WriteLine("Большее число : "+ num1); 
+   Console.WriteLine("Меньшее число : "+ num2);
+}
+else
+{
+Console.WriteLine("Большее число : "+ num2);
+Console.WriteLine("Большее число : "+ num1);
+}
+
