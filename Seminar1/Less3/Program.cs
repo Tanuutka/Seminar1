7a946c740aadf306a8f03c87d1897b8d320393b4
Tanuutka <Tanuutka@yandex.ru>
2022-10-16 13:08:37 +0300

    написана программа определения четного или нечетного чмсла

---
+++ Seminar1/Less3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+int a = number % 2;
+if (a == 0)
+{
+  Console.WriteLine("Число является четным  ");
+}
+else
+{
+    Console.WriteLine("число является нечетным ");
+}
+
+
