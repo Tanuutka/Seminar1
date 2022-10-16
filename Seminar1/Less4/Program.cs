141933f8e531a328fbb8a86628a256570bdfa3bb
Tanuutka <Tanuutka@yandex.ru>
2022-10-16 21:22:35 +0300

    написана программа выведения всех четных чисел до n

---
+++ Seminar1/Less4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+Console.Clear();
+int i = -2;
+while (i < number-1)
+{
+    i+=2;
+
+   Console.Write(" "+ i) ;
+}
