a6d01b73c2eefa15ead44ef215b80e5f19b8b869
Tanuutka <Tanuutka@yandex.ru>
2022-10-17 08:01:46 +0300

    написана программа выведения всех четных чисел до n

--- Seminar1/Less4/Program.cs
+++ Seminar1/Less4/Program.cs
@@ -1,7 +1,9 @@
-﻿Console.WriteLine("Введите число: ");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32 (Console.ReadLine());
 Console.Clear();
-int i = -2;
+int i = 0;
 while (i < number-1)
 {
     i+=2;
